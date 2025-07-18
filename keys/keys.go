59f426b15fb803e025e3df776fa93bc7bbea9a1c
Austin Amoruso <austin@yardstick.team>
2025-07-18 14:12:40 -0700

    Add ctrl+h keybinding for viewing history

--- keys/keys.go
+++ keys/keys.go
@@ -222,6 +222,9 @@ var GlobalkeyBindings = map[KeyName]key.Binding{
 	KeyBookmark: key.NewBinding(
 		key.WithKeys("B"),
 		key.WithHelp("B", "bookmark"),
+	KeyHistory: key.NewBinding(
+		key.WithKeys("ctrl+h"),
+		key.WithHelp("ctrl+h", "view history"),
 	),
 
 	// -- Special keybindings --
