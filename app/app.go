8a6a86d8c53d3b48bc2292a6ff3edc3135c8cfca
Austin Amoruso <austin@yardstick.team>
2025-07-18 12:28:47 -0700

    Add keybinding editor state handling to home view

--- app/app.go
+++ app/app.go
@@ -453,6 +453,10 @@ func (m *home) handleKeyPress(msg tea.KeyMsg) (mod tea.Model, cmd tea.Cmd) {
 		return m.handleHistoryState(msg)
 	}
 
+	if m.state == stateKeybindingEditor {
+		return m.handleKeybindingEditorState(msg)
+	}
+
 	if m.state == stateNew {
 		// Handle quit commands first. Don't handle q because the user might want to type that.
 		if msg.String() == "ctrl+c" {
