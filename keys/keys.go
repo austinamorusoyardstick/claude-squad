ccd1a28dac263a67babd9f6f4ea123c08d6b32a9
Austin Amoruso <austin@yardstick.team>
2025-07-28 13:54:12 -0700

    Add KeyCheckUpdate constant for update checking functionality

--- keys/keys.go
+++ keys/keys.go
@@ -63,6 +63,7 @@ const (
 	KeyEditKeybindings // Key for opening keybinding editor
 	KeyGitStatus       // Key for showing git status overlay
 	KeyGitStatusBookmark // Key for showing git status overlay in bookmark mode
+	KeyCheckUpdate     // Key for checking for updates
 )
 
 // GlobalKeyStringsMap is a global, immutable map string to keybinding.
