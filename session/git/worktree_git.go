6af61cc34f79894d1749c3b454ff52b469d23684
Austin Amoruso <austin@yardstick.team>
2025-07-29 15:32:56 -0700

    Improve git reset logging to distinguish between new and existing backups

--- session/git/worktree_git.go
+++ session/git/worktree_git.go
@@ -352,7 +352,11 @@ func (g *GitWorktree) ResetToOrigin() error {
 		return fmt.Errorf("failed to reset to origin/%s. Backup branch created: %s. Error: %w", g.branchName, backupBranch, err)
 	}
 
-	log.InfoLog.Printf("Successfully reset branch %s to origin/%s. Backup branch: %s", g.branchName, g.branchName, backupBranch)
+	if isBackedUp {
+		log.InfoLog.Printf("Successfully reset branch %s to origin/%s. Using existing backup: %s", g.branchName, g.branchName, backupBranch)
+	} else {
+		log.InfoLog.Printf("Successfully reset branch %s to origin/%s. New backup branch: %s", g.branchName, g.branchName, backupBranch)
+	}
 	return nil
 }
 
