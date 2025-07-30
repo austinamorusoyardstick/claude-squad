9f4d10aa2940d6c9c1e999036cca6bf30f0a7825
Austin Amoruso <austin@yardstick.team>
2025-07-30 14:00:01 -0700

    Add 'R' keybinding to filter and show only reviews in PR review UI

--- ui/pr_review.go
+++ ui/pr_review.go
@@ -178,6 +178,14 @@ func (m PRReviewModel) Update(msg tea.Msg) (PRReviewModel, tea.Cmd) {
 			m = m.resetViewAfterFilterChange()
 			return m, nil
 
+		case "R":
+			// Show only reviews
+			m.showComments = false
+			m.showReviews = true
+			m.showOnlyLineComments = false
+			m = m.resetViewAfterFilterChange()
+			return m, nil
+
 		case "ctrl+r":
 			// Resolve all conversations
 			return m, func() tea.Msg { return PRResolveAllConversationsMsg{} }
